f3c6b093c564f43375e7153e9a34224c75eba6c2
gwatic <gwatic11@gmail.com>
2024-04-04 22:13:21 +0700

    added handler auth when logged in

--- src/features/authentication/pages/login.tsx
+++ src/features/authentication/pages/login.tsx
@@ -1,7 +1,30 @@
+import { useAuthStore } from '@/shared/stores/user-store';
 import LoginForm from '../components/login-form';
 import LogoImg from '@/assets/logo.jpg';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const user = useAuthStore((state) => state.user);
+
+  const handleAuth = () => {
+    if (!user) return;
+
+    if (user.role.name === 'student') {
+      navigate('/student/home', { replace: true });
+
+      return;
+    }
+
+    navigate('/dashboard');
+  };
+
+  useEffect(() => {
+    handleAuth();
+  }, [user]);
+
   return (
     <div className='container mx-auto flex min-h-screen items-center justify-center'>
       <div className='grid grid-cols-1 place-content-center gap-10 lg:grid-cols-2 lg:gap-20'>
