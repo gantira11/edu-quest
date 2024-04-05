91e7d11abdb7e3bd676df0946d0f707ddfc5734b
gwatic <gwatic11@gmail.com>
2024-04-05 22:29:31 +0700

    fixing games

--- src/features/student/pages/simulation.tsx
+++ src/features/student/pages/simulation.tsx
@@ -17,7 +17,7 @@ const Simulation = () => {
       <Breadcrumbs items={breadcrumbs} />
 
       <iframe
-        src='./games/circuit-construction-kit-ac-virtual-lab_en.html'
+        src={window.location.href + '/games/circuit-construction-kit-ac-virtual-lab_en.html'}
         allowFullScreen
         title='game'
         width='100%'
