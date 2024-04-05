d5eacaecd9750e1c82a2d322e25bd587aa4cf9f2
gwatic <gwatic11@gmail.com>
2024-04-05 21:41:12 +0700

    adjust pwa strategies

--- vite.config.ts
+++ vite.config.ts
@@ -50,6 +50,10 @@ export default defineConfig({
             handler: 'CacheFirst',
             options: {
               cacheName: 'iframe-cache',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24 * 365
+              },
             },
           },
           {
@@ -57,11 +61,15 @@ export default defineConfig({
             handler: 'CacheFirst',
             options: {
               cacheName: 'firebase-storage-cache',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24 * 365
+              },
             },
           },
           {
             urlPattern: /\**\/*/,
-            handler: 'CacheFirst',
+            handler: 'NetworkFirst',
             method: 'GET',
             options: {
               cacheName: 'all-cache',
