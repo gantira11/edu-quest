8cb44e3b65ec52f9aaec5d098636a5eab9f94b79
gwatic <gwatic11@gmail.com>
2024-04-10 03:01:11 +0700

    adjust auth when logged in

--- src/shared/layouts/base-layout.tsx
+++ src/shared/layouts/base-layout.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 import PageLoader from '../components/page-loader';
+import { useAuthStore } from '../stores/user-store';
 
 const BaseLayout = () => {
+  const navigate = useNavigate();
+
+  const user = useAuthStore((state) => state.user);
+
+  const handleAuth = () => {
+    if (user?.role.name === 'student') {
+      navigate('/student/home', { replace: true });
+
+      return;
+    }
+
+    navigate('/dashboard');
+  };
+
+  useEffect(() => {
+    console.log(user, 'USER HERE');
+    if (user) {
+      handleAuth();
+    }
+  }, [user]);
+
   return (
     <section>
       <React.Suspense fallback={<PageLoader />}>
