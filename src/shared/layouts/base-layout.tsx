668eef273f332e918757b3b68620beeedae1564e
gwatic <gwatic11@gmail.com>
2024-04-11 19:59:20 +0700

    fixed auth

--- src/shared/layouts/base-layout.tsx
+++ src/shared/layouts/base-layout.tsx
@@ -1,14 +1,18 @@
 import React, { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import PageLoader from '../components/page-loader';
+import { useAuthStore } from '../stores/user-store';
 
 const BaseLayout = () => {
   const navigate = useNavigate();
+  const persistUser = useAuthStore((state) => state.persistUser);
 
   const handleAuth = () => {
     const storageUser = localStorage.getItem('user');
     if (storageUser !== null) {
       const user = JSON.parse(storageUser);
+      persistUser(user);
+
       if (user.role.name === 'student') {
         navigate('/student/home', { replace: true });
         return;
